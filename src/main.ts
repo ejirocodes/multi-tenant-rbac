52951ab72c891886f924dea28f50c1ecaf60bebf
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2023-10-23 19:29:37 +0100

    feat: postgres drizzle database connection and application schema

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { env } from "./config/env";
+import { db } from "./db";
 import { logger } from "./util/logger";
 import { buildServer } from "./util/server";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 
 async function gracefulShutDown({
   app,
@@ -18,6 +20,9 @@ async function main() {
     host: env.HOST,
   });
 
+  migrate(db, {
+    migrationsFolder: "./migrations",
+  });
   const signals = ["SIGINT", "SIGTERM", "SIGQUIT"] as const;
 
   for await (const signal of signals) {
